3326766c445e2a65faf4d9db63d34b9b7fb4ea6c
Mohamed Gomaa <mohamedgomaabakri@gmail.com>
2026-04-15 14:32:06 +0200

    .

--- lib/main.dart
+++ lib/main.dart
@@ -6,11 +6,12 @@ import 'package:flutter/services.dart';
 import 'package:flutter/material.dart';
 import 'package:clean_arc/core/injection/injection_app.dart' as di;
 
-class MyHttpOverrides extends HttpOverrides{
+class MyHttpOverrides extends HttpOverrides {
   @override
-  HttpClient createHttpClient(SecurityContext? context){
+  HttpClient createHttpClient(SecurityContext? context) {
     return super.createHttpClient(context)
-      ..badCertificateCallback = (X509Certificate cert, String host, int port)=> true;
+      ..badCertificateCallback =
+          (X509Certificate cert, String host, int port) => true;
   }
 }
 
@@ -18,7 +19,7 @@ void main() async {
   await di.init();
   // await DioHelper.init();
   // await AppStorage.init();
-   WidgetsFlutterBinding.ensureInitialized();
+  WidgetsFlutterBinding.ensureInitialized();
   await EasyLocalization.ensureInitialized();
   // await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   // await FlutterLocalNotificationHelper.init();
@@ -30,20 +31,21 @@ void main() async {
   runApp(
     EasyLocalization(
       useOnlyLangCode: true,
-      supportedLocales: const [ Locale('ar'),Locale('en')],
+      supportedLocales: const [Locale('ar'), Locale('en')],
       fallbackLocale: const Locale('ar'),
-      startLocale:const Locale('en') ,
+      startLocale: const Locale('en'),
       path: 'assets/translations',
-      assetLoader: CodegenLoader(),
-      child:const MainApp(),
+      assetLoader: const CodegenLoader(),
+      child: const MainApp(),
     ),
   );
 
-  SystemChrome.setSystemUIOverlayStyle( SystemUiOverlayStyle(
-    statusBarColor: Colors.black,));
-  SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual, overlays: [SystemUiOverlay.top]);
+  SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
+    statusBarColor: Colors.black,
+  ));
+  SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual,
+      overlays: [SystemUiOverlay.top]);
   SystemChrome.setPreferredOrientations([
     DeviceOrientation.portraitUp,
   ]);
 }
-
